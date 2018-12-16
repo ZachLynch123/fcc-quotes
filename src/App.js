eebde3e6291983acc702c34558e5a4bfc755b3d5
HankBedford <Zachary.Lynch1337@gmail.com>
2018-12-16 10:59:19 -0800

    Asynch request using axious to get quotes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      quotes: [],
+      /* colors: ['#000033', '#696969', 
+        '#c0ffee', '#b7410e', '#4d3c63'] */
+    }
+  }
+  
+
+  componentDidMount() {
+ 
+    Axios.get('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+      .then(res => {
+        const quoteArray = res.data.quotes;
+        this.setState({
+          quotes: quoteArray
+        });
+        console.log(this.state.quotes)
+      })
+        
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
       </div>
     );
   }
